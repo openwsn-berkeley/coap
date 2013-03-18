cd52a7cafbed4347a4a2d531d9e083a5bb557380
Thomas Watteyne <twatteyne@gmail.com>
2013-03-17 22:06:48 -0700

    Adding version file and README.

---
+++ README.txt
@@ -0,0 +1,25 @@
+CoAP Python library
+-------------------
+
+This package implements the Constrained Application Protocol (CoAP)
+developed by the IETF CORE working group.
+
+This package aims at implementing:
+http://tools.ietf.org/html/draft-ietf-core-coap-13
+
+In particular, it implements:
+- a CoAP client
+- a CoAP server with an arbitrary number of resources
+
+It does NOT currently implement:
+- CoAP proxying
+- CoAP caching
+- HTTP-to-CoAP or CoAP-to-HTTP cross-proxying
+
+This package contains Doxygen-based HTML documentation.
+
+More documentation at http://www.openwsn.org/.
+Report bugs at https://openwsn.atlassian.net/browse/COAP.
+
+This package is part of Berkeley's OpenWSN project,
+http://www.openwsn.org/.
--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from distutils.core import setup
 
+# retrieve the version number
+from coap import coapVersion
+VERSION     = '.'.join([str(v) for v in coapVersion.VERSION])
+with open('README.txt') as f
+    LONG_DESCRIPTION = f.readlines()
+
 setup(
     name           = "coap",
     packages       = ["coap"],
-    version        = "1.0.0",
+    version        = VERSION,
     author         = "Thomas Watteyne",
     author_email   = "watteyne@eecs.berkeley.edu",
     description    = "A CoAP Python library",
@@ -26,31 +32,5 @@ setup(
         "Topic :: Software Development :: Libraries",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
-    long_description = """\
-CoAP Python library
--------------------
-
-This package implements the Constrained Application Protocol (CoAP)
-developed by the IETF CORE working group.
-
-This package aims at implementing:
-http://tools.ietf.org/html/draft-ietf-core-coap-13
-
-In particular, it implements:
-- a CoAP client
-- a CoAP server with an arbitrary number of resources
-
-It does NOT currently implement:
-- CoAP proxying
-- CoAP caching
-- HTTP-to-CoAP or CoAP-to-HTTP cross-proxying
-
-This package contains Doxygen-based HTML documentation.
-
-More documentation at http://www.openwsn.org/.
-Report bugs at https://openwsn.atlassian.net/browse/COAP.
-
-This package is part of Berkeley's OpenWSN project,
-http://www.openwsn.org/.
-"""
+    long_description = LONG_DESCRIPTION,
 )
