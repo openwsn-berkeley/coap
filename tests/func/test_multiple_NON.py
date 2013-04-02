ca745f273563199874456bab95621850f43dbcf5
Thomas Watteyne <twatteyne@gmail.com>
2013-04-01 22:37:52 -0700

    Multiple CON and NON functional tests.

---
+++ tests/func/test_multiple_NON.py
@@ -0,0 +1,29 @@
+import logging
+import testUtils as utils
+
+import time
+import threading
+
+from conftest import IPADDRESS1, \
+                     RESOURCE, \
+                     DUMMYVAL
+from coap     import coapDefines as d
+
+#============================ logging ===============================
+
+log = logging.getLogger(utils.getMyLoggerName())
+log.addHandler(utils.NullHandler())
+    
+#============================ tests ===========================================
+
+def test_GET(logFixture,snoopyDispatcher,twoEndPoints):
+    
+    (coap1,coap2) = twoEndPoints
+    
+    # have coap2 do a get
+    for _ in range(20):
+        reply = coap2.GET(
+            uri         = 'coap://[{0}]:{1}/{2}/'.format(IPADDRESS1,d.DEFAULT_UDP_PORT,RESOURCE),
+            confirmable = False,
+        )
+        assert reply==DUMMYVAL
