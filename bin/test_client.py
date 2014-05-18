50426b292ae628cbd4122e81084ce95dd701cad6
Thomas Watteyne <twatteyne@gmail.com>
2014-05-18 07:29:17 +0200

    COAP-9. Moving CoAP client to different UDP port.

--- bin/test_client.py
+++ bin/test_client.py
@@ -3,19 +3,25 @@ import sys
 here = sys.path[0]
 sys.path.insert(0, os.path.join(here,'..'))
 
-import logging_setup
+import time
+
 from   coap import coap
+import logging_setup
 
 SERVER_IP = '::1'
 
 # open
-c = coap.coap()
+c = coap.coap(udpPort=5000)
 
 # retrieve value of 'test' resource
 p = c.GET('coap://[{0}]/test'.format(SERVER_IP),)
-print p
+print '====='
+print ''.join([chr(b) for b in p])
+print '====='
 
 # close
 c.close()
+
+time.sleep(0.500)
 
 raw_input("Done. Press enter to close.")
