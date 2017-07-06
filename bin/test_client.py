328ddf851fbea360b079d1701c4f220597f05880
Mališa Vučinić <malishav@gmail.com>
2017-07-06 14:32:30 +0200

    COAP-36. Add examples.

--- bin/test_client.py
+++ bin/test_client.py
@@ -4,8 +4,12 @@ here = sys.path[0]
 sys.path.insert(0, os.path.join(here,'..'))
 
 import time
+import binascii
 
 from   coap import coap
+from coap import coapOption           as o
+from coap import coapObjectSecurity   as oscoap
+
 import logging_setup
 
 SERVER_IP = '::1'
@@ -13,11 +17,24 @@ SERVER_IP = '::1'
 # open
 c = coap.coap(udpPort=5000)
 
-# retrieve value of 'test' resource
-p = c.GET('coap://[{0}]/test'.format(SERVER_IP),)
-print '====='
-print ''.join([chr(b) for b in p])
-print '====='
+context = oscoap.SecurityContext(masterSecret=binascii.unhexlify('000102030405060708090A0B0C0D0E0F'),
+                                 senderID=binascii.unhexlify('636c69656e74'),
+                                 recipientID=binascii.unhexlify('736572766572'),
+                                 aeadAlgorithm=oscoap.AES_CCM_16_64_128())
+
+objectSecurity = o.ObjectSecurity(context=context)
+
+try:
+    # retrieve value of 'test' resource
+    p = c.GET('coap://[{0}]/test'.format(SERVER_IP),
+              confirmable=True,
+              options=[objectSecurity])
+
+    print '====='
+    print ''.join([chr(b) for b in p])
+    print '====='
+except Exception as err:
+    print err
 
 # close
 c.close()
--- bin/test_server.py
+++ bin/test_server.py
@@ -4,9 +4,11 @@ here = sys.path[0]
 sys.path.insert(0, os.path.join(here,'..'))
 
 import threading
-from   coap   import    coap,                    \
-                        coapResource,            \
-                        coapDefines as d
+import binascii
+from   coap   import    coap,                            \
+                        coapResource,                    \
+                        coapDefines         as d,        \
+                        coapObjectSecurity  as oscoap
 import logging_setup
 
 class testResource(coapResource.coapResource):
@@ -24,15 +26,25 @@ class testResource(coapResource.coapResource):
         
         respCode        = d.COAP_RC_2_05_CONTENT
         respOptions     = []
-        respPayload     = [ord(b) for b in 'dummy response']
+        respPayload     = [ord(b) for b in 'hello world 1 2 3 4 5 6 7 8 9 0']
         
         return (respCode,respOptions,respPayload)
 
 # open
 c = coap.coap(ipAddress='::1')
 
+testResource = testResource()
+
+context = oscoap.SecurityContext(masterSecret   = binascii.unhexlify('000102030405060708090A0B0C0D0E0F'),
+                                 senderID       = binascii.unhexlify('736572766572'),
+                                 recipientID    = binascii.unhexlify('636c69656e74'),
+                                 aeadAlgorithm  = oscoap.AES_CCM_16_64_128())
+
+# add resource - context binding with authorized methods
+testResource.addSecurityBinding((context, d.METHOD_ALL))
+
 # install resource
-c.addResource(testResource())
+c.addResource(testResource)
 
 for t in threading.enumerate():
     print t.name
