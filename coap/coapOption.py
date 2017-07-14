9227242524bcc1ce9c2da7a1dcb11523ed741b8b
Mališa Vučinić <malishav@gmail.com>
2017-07-14 14:05:16 +0200

    COAP-37. Add accept option.

--- coap/coapOption.py
+++ coap/coapOption.py
@@ -138,6 +138,24 @@ class ContentFormat(coapOption):
 
 #=== OPTION_NUM_ACCEPT
 
+class Accept(coapOption):
+    def __init__(self, accept):
+        assert len(accept) == 1
+        assert accept[0] in d.FORMAT_ALL
+
+        # initialize parent
+        coapOption.__init__(self, d.OPTION_NUM_ACCEPT, d.OSCOAP_CLASS_E)
+
+        # store params
+        self.accept = accept[0]
+
+    def __repr__(self):
+        return 'Accept(format={0})'.format(self.accept)
+
+    def getPayloadBytes(self):
+        return [self.accept]
+
+
 #=== OPTION_NUM_LOCATIONQUERY
 
 #=== OPTION_NUM_BLOCK2
